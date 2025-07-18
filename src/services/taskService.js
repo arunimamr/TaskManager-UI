bbf305694feb742ae45faa983694d6d7b98b6531
Arunima <arunimamr2020@gmail.com>
2025-07-18 14:11:42 +0400

    configuration change for deployment in railway

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,29 +1,29 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/tasks';
+const BASE_URL = process.env.REACT_APP_API_URL;
 
     export const getAllTasks = (taskStatus="", pageNum=1, pageSize=5) => {
-        let url = `${BASE_URL}/all?pageNum=${pageNum}&pageSize=${pageSize}`;
+        let url = `${BASE_URL}/tasks/all?pageNum=${pageNum}&pageSize=${pageSize}`;
         if(taskStatus) 
             url = url + `&status=${taskStatus}`;
             return axios.get(url);
     }    
   
     export const getTaskById = (id) => {
-        return axios.get(`${BASE_URL}/${id}`);
+        return axios.get(`${BASE_URL}/tasks/${id}`);
     }
 
 
     export const createTask = (task) => {
-        return axios.post(`${BASE_URL}/save`, task);
+        return axios.post(`${BASE_URL}/tasks/save`, task);
     }
 
 
     export const updateTask = (id, task) => {
-        return axios.put(`${BASE_URL}/${id}`, task)
+        return axios.put(`${BASE_URL}/tasks/${id}`, task)
     }
 
     export const deleteTask= (id) => {
         console.log("delete id :  " + id)
-        axios.delete(`${BASE_URL}/${id}`);
+        axios.delete(`${BASE_URL}/tasks/${id}`);
     }
